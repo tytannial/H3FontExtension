7ee6e45ce9deec27afc4459e19695647fa69be98
Tytannial <Tytannial@users.noreply.github.com>
2022-09-16 11:26:03 +0800

    Only inject to main game exe

--- H3CN/dllmain.cpp
+++ H3CN/dllmain.cpp
@@ -2,6 +2,18 @@
 
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved)
 {
+    TCHAR pName[MAX_PATH];
+    if (!GetModuleFileName(NULL, pName, MAX_PATH))
+    {
+        return FALSE;
+    }
+    auto pEnd = wcsrchr(pName, '\\');
+
+    if (_wcsicmp(pEnd, L"\\h3hota HD.exe") != 0 && _wcsicmp(pEnd, L"\\Heroes3 HD.exe") != 0)
+    {
+        return FALSE;
+    }
+
     switch (ul_reason_for_call)
     {
     case DLL_PROCESS_ATTACH:
